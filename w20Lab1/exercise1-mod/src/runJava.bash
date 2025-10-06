a8bde2c64c3a921b612ab23dacb79980a0511577
Fregaladol <friigaiden@gmail.com>
2025-10-06 22:30:34 +0200

    Add files via upload

---
+++ w20Lab1/exercise1-mod/src/runJava.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# compilar codigo
+javac App.java
+
+
+# ejecutar y pasar argumentos
+java App "EjEcutAR pRogrAmA" 1 
+java App "EjEcutAR pRogrAmA" 2
+java App "EjEcutAR pRogrAmA" 3
+java App "EjEcutAR pRogrAmA" 
